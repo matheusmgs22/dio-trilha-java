13beb1e0f954404e3bb5f05ffca2a5f0bbc2e773
matheusmgs22 <matheusmgs17@gmail.com>
2024-08-20 08:23:08 -0300

    condição ternária

--- controle-de-fluxo/src/ResultadoEscolar.java
+++ controle-de-fluxo/src/ResultadoEscolar.java
@@ -2,15 +2,9 @@ public class ResultadoEscolar {
 
     public static void main(String[] args) {
 
-        int nota = 5;
+        int nota = 8;
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "recuperação" : "Reprovado";
 
-        if (nota >= 7) {
-            System.out.println("Aprovado");
-        } else if (nota >= 5 && nota < 7) {
-            System.out.println("Recuperação");
-        } else {
-            System.out.println("Reprovado");
-        }
+        System.out.println(resultado);
     }
-
 }
