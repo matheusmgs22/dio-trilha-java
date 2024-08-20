ebda85c6746b0d533b8d7e715ee02e81300a9ef9
matheusmgs22 <matheusmgs17@gmail.com>
2024-08-20 09:28:17 -0300

    for com array

---
+++ estruturas-de-repeticao/src/ExemploForArray.java
@@ -0,0 +1,10 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos[] = { "Felipe", "Jonas", "Julia", "Marcos" };
+
+        for (int x = 0; x < alunos.length; x++) {
+            System.out.println("O aluno no indice x = " + x + " é " + alunos[x]);
+
+        }
+    }
+}
