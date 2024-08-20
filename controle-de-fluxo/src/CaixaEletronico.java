656107152719de1068c6b3ff8a6b39dec5256ca4
matheusmgs22 <matheusmgs17@gmail.com>
2024-08-20 08:13:19 -0300

    condição encadeada

---
+++ controle-de-fluxo/src/CaixaEletronico.java
@@ -0,0 +1,17 @@
+public class CaixaEletronico {
+
+    public static void main(String[] args) {
+
+        double saldo = 25.0;
+        double valorSolicitado = 26.0;
+
+        if (valorSolicitado <= saldo) {
+
+            saldo = saldo - valorSolicitado;
+            System.out.println(saldo);
+        } else {
+            System.out.println("saldo insuficiente");
+        }
+
+    }
+}
---
+++ controle-de-fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,16 @@
+public class ResultadoEscolar {
+
+    public static void main(String[] args) {
+
+        int nota = 5;
+
+        if (nota >= 7) {
+            System.out.println("Aprovado");
+        } else if (nota >= 5 && nota < 7) {
+            System.out.println("Recuperação");
+        } else {
+            System.out.println("Reprovado");
+        }
+    }
+
+}
