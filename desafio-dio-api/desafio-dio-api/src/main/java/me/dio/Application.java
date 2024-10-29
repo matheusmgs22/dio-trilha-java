d6b01d19640ff4b31019c175c570b9a9274c5b21
matheusmgs22 <matheusmgs17@gmail.com>
2024-10-29 15:18:04 -0300

    Desafio API completo

---
+++ desafio-dio-api/desafio-dio-api/src/main/java/me/dio/Application.java
@@ -0,0 +1,15 @@
+package main.java.me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL") })
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ desafio-dio-api/desafio-dio-api/src/main/java/me/dio/domain/model/Feature.java
@@ -0,0 +1,8 @@
+package main.java.me.dio.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+
+}
